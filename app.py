da86816975d2a6bfeb94bdad1d5d2eaeb0482984
santhoshworkspace26 <santhoshk8470@gmail.com>
2026-06-14 12:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 latest_script = ""
 
